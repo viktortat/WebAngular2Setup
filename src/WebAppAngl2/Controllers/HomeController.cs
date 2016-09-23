5794a06df3b639b0c7656086b618191c08581e27
Виктор <viktortat@gmail.com>
2016-09-23 22:47:31 +0300

    2

--- src/WebAppAngl2/Controllers/HomeController.cs
+++ src/WebAppAngl2/Controllers/HomeController.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Cli.Utils;
 
 namespace WebAppAngl2.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Angular2()
         {
             return View();
         }
